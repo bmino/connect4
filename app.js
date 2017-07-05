67c171f327accc427262cd4ea9c7e5249263e17a
Unknown <brandonmino@bellsouth.net>
2017-07-05 17:11:16 -0400

    Hacking Away - Some Multiplayer
    Tidbits of multiplayer interaction. Better grid drawing.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ server.listen(2000);
 
 
 var SOCKET_LIST = [];
+var GAME_LIST = [];
 
 var io = require('socket.io')(server, {});
 io.sockets.on('connect', function(socket) {
@@ -19,10 +20,61 @@ io.sockets.on('connect', function(socket) {
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
 
+    /* Tries to join existing game if possible */
+    if (!joinGame(socket)) {
+        var createdGame = createNewGame(socket);
+    }
+
+    socket.on('move:make', function(move) {
+        var gameId = move.game;
+        var game = GAME_LIST[gameId];
+        // Invalid game id
+        if (!game) {
+            console.log('Err:move - could not find game id ' + gameId);
+            return;
+        }
+        // Space is already been taken
+        if (game.grid[move.col][move.row]) {
+            console.log('Err: move - (' + move.col + ', ' + move.row + ') has already been made');
+            return;
+        }
+        game.grid[move.col][move.row] = gameId;
+        game.host.emit('move:draw', move);
+        game.opponent.emit('move:draw', move);
+    });
+
+
     socket.on('disconnect', function() {
         delete SOCKET_LIST[socket.id];
+        // Notify games player was a part of
     });
 
 });
 
 
+function joinGame(socket) {
+    for (var gameId in GAME_LIST) {
+        var game = GAME_LIST[gameId];
+        if (game.opponent === null) {
+            // Can join
+            game.opponent = socket;
+            socket.emit('game:join', {id: gameId});
+            return true;
+        }
+    }
+    // No games available to join
+    return false;
+}
+
+function createNewGame(socket) {
+    /* Create New Game */
+    var game = {
+        host: socket,
+        opponent: null,
+        grid: [[], [], []]
+    };
+    var gameId = Math.random();
+    GAME_LIST[gameId] = game;
+    socket.emit('game:host', {id: gameId});
+    return game;
+}
--- public/js/bmDraw.js
+++ public/js/bmDraw.js
@@ -13,14 +13,15 @@ var bmDraw = {
             count: 0,
             width: 0,
             border: 0
-        }
+        },
+        state: []
     },
 
-    init: function (canvas, ctx) {
+    init: function (canvas, ctx, border) {
         this.canvas = canvas;
         this.ctx = ctx;
-        this.grid.cols.border = 3;
-        this.grid.rows.border = 3;
+        this.grid.cols.border = border;
+        this.grid.rows.border = border;
         this.drawGrid(3, 3);
     },
 
@@ -35,6 +36,14 @@ var bmDraw = {
         this.grid.rows.count = rows;
         this.grid.rows.height = (height - ((rows-1) * this.grid.rows.border)) / rows;
         this.debugger('grid.rows.height: ' + this.grid.rows.height);
+
+        this.grid.state = [];
+        for (var c=0; c<cols; c++) {
+            this.grid.state[c] = [];
+            for (var r=0; r<rows; r++) {
+                this.grid.state[c][r] = '';
+            }
+        }
 
         var x = 0;
         for (var i=0; i<cols-1; i++) {
@@ -56,27 +65,52 @@ var bmDraw = {
         var corner = this.getCorner(col, row);
         var width = this.grid.cols.width;
         var height = this.grid.rows.height;
-        this.debugger('marking corner: (' + corner.x + ', ' + corner.y + ')');
-        this.debugger('marking width: ' + width);
-        this.debugger('marking height: ' + height);
+        //this.debugger('marking corner: (' + corner.x + ', ' + corner.y + ')');
+        //this.debugger('marking width: ' + width);
+        //this.debugger('marking height: ' + height);
 
         this.ctx.fillStyle = '#EE0000';
         this.ctx.fillRect(corner.x, corner.y, width, height);
         this.ctx.fillStyle = originalColor;
+
+        this.grid.state[col][row] = 'X';
+        this.debug && this.debugGrid();
+    },
+
+    identifyGridFromMouse: function(x, y) {
+        this.debugger('Identifying grid at mouse position (' + x + ', ' + y + ')');
+        var col = Math.floor(x / (this.canvas.scrollWidth / this.grid.cols.count));
+        col = Math.min(col, this.grid.cols.count-1);
+        var row = Math.floor(y / (this.canvas.scrollHeight / this.grid.rows.count));
+        row = Math.min(row, this.grid.rows.count-1);
+
+        this.debugger('Identified grid position (' + col + ', ' + row + ')');
+
+        return {x: col, y: row};
     },
 
     getCorner: function (col, row) {
-        var x = (this.grid.cols.width * col);
-        x += this.grid.cols.border * (col);
-        var y = (this.grid.rows.height * row);
-        y += this.grid.rows.border * (row);
-        return {
-            x: x,
-            y: y
-        };
+        var x = (this.grid.cols.width * col) + (this.grid.cols.border * col);
+        var y = (this.grid.rows.height * row) + (this.grid.rows.border * row);
+        return {x: x, y: y};
     },
 
     debugger: function(msg) {
         this.debug && console.log(msg);
+    },
+
+    debugGrid: function() {
+        if (!this.debug) return;
+
+        var rowString = '';
+        for (var c=0; c<this.grid.cols.count; c++) {
+            rowString = '[';
+            for (var r=0; r<this.grid.rows.count; r++) {
+                if (this.grid.state[r][c].length === 0) rowString += '-';
+                else rowString += this.grid.state[r][c];
+            }
+            rowString += ']';
+            console.log(rowString);
+        }
     }
 };
